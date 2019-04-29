15b6e3532bff94ca5a22c6282e6855281a9f6244
mikegjeff <mikegjeffcoat@gmail.com>
2019-04-30 11:34:57 +1200

    state-alert-working

--- src/components/LoginPage.react.js
+++ src/components/LoginPage.react.js
@@ -3,12 +3,21 @@ import { Formik } from 'formik'
 import { LoginPage as TablerLoginPage } from 'tabler-react'
 import axios from 'axios'
 import API from '../utils/API'
-import { Page, Grid } from 'tabler-react'
+import { Page, Grid, Alert } from 'tabler-react'
 
 export default class LoginPage extends Component {
 	constructor(props) {
 		super(props)
+		this.state = {
+			message: ''
+		}
 	}
+
+	handleChange = async (values, { resetForm, setErrors, setSubmitting }) => {
+		const state = this.state
+		this.setState(state)
+	}
+
 	handleSubmit = async (values, { resetForm, setErrors, setSubmitting }) => {
 		console.log(values)
 		setSubmitting(true)
@@ -23,63 +32,79 @@ export default class LoginPage extends Component {
 			})
 			.catch(error => {
 				if (error.response.status === 409) {
-					alert('Email and password do not match')
+					this.setState({
+						message: 'Login failed. Username or password not match'
+					})
 				}
 				if (error.response.status === 404) {
-					alert('User not found')
+					this.setState({
+						message: 'User Not Found'
+					})
 				}
 			})
 		return
 	}
 	render() {
+		const { message } = this.state
 		return (
 			<Page.Content>
-				<Grid.Row cards={true}>
-					<Formik
-						initialValues={{
-							email: '',
-							password: ''
-						}}
-						validate={values => {
-							let errors = {}
-							if (!values.email) {
-								errors.email = 'Required'
-							} else if (
-								!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-							) {
-								errors.email = 'Invalid email address'
-							} else if (
-								values.email
-									.split('@')
-									.slice(1)
-									.indexOf('example.com') === -1
-							) {
-								errors.email = 'Not a valid AUT Email'
-							}
-
-							return errors
-						}}
-						onSubmit={this.handleSubmit}
-						render={({
-							values,
-							errors,
-							touched,
-							handleChange,
-							handleBlur,
-							handleSubmit,
-							isSubmitting
-						}) => (
-							<TablerLoginPage
-								onSubmit={handleSubmit}
-								onChange={handleChange}
-								onBlur={handleBlur}
-								values={values}
-								errors={errors}
-								touched={touched}
-							/>
+				<Grid.Col md={6}>
+					<Grid.Row cards={true}>
+						<Formik
+							initialValues={{
+								email: '',
+								password: ''
+							}}
+							validate={values => {
+								let errors = {}
+								if (!values.email) {
+									errors.email = 'Required'
+								} else if (
+									!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(
+										values.email
+									)
+								) {
+									errors.email = 'Invalid email address'
+								} else if (
+									values.email
+										.split('@')
+										.slice(1)
+										.indexOf('example.com') === -1
+								) {
+									errors.email = 'Not a valid AUT Email'
+								}
+								return errors
+							}}
+							onSubmit={this.handleSubmit}
+							onChange={this.handleChange}
+							render={({
+								values,
+								errors,
+								touched,
+								handleChange,
+								handleBlur,
+								handleSubmit,
+								isSubmitting
+							}) => (
+								<TablerLoginPage
+									onSubmit={handleSubmit}
+									onChange={handleChange}
+									onBlur={handleBlur}
+									values={values}
+									errors={errors}
+									touched={touched}
+								/>
+							)}
+						/>
+					</Grid.Row>
+					<Grid.Row>
+						{message !== '' && (
+							<Alert type="primary">
+								<Alert.Link>{message}</Alert.Link>
+							</Alert>
 						)}
-					/>
-				</Grid.Row>
+					</Grid.Row>
+				</Grid.Col>
 			</Page.Content>
 		)
 	}
