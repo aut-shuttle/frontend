45665423fb593ca4c0f22d030a6eff81f34485e2
mikegjeff <mikegjeffcoat@gmail.com>
2019-05-01 15:36:01 +1200

    login working with register button, formatted as a card, tabler, and individual elements

--- src/components/LoginPage.react.js
+++ src/components/LoginPage.react.js
@@ -25,6 +25,15 @@ export default class LoginPage extends Component {
 		this.setState(state)
 	}
 
+	handleValidate = values => {
+		const errors = {}
+		if (!values.email) errors.email = 'Required'
+		if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+			errors.email = 'You must supply a valid email address'
+		}
+		return errors
+	}
+
 	handleSubmit = async (values, { setSubmitting }) => {
 		console.log(values)
 		setSubmitting(true)
@@ -58,106 +67,78 @@ export default class LoginPage extends Component {
 		const { message } = this.state
 		return (
 			<Page.Content>
-				<Grid.Col md={10}>
-					<Grid.Row>
-						<Alert type="primary">
-							<Alert.Link>Welcome to the AUT Shuttle App!</Alert.Link>
-						</Alert>
-					</Grid.Row>
-					<Grid.Row cards={true}>
-						<Formik
-							initialValues={{ email: '', password: '' }}
-							validate={values => {
-								const errors = {}
-
-								if (!values.email) errors.email = 'Required'
-
-								if (
-									!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(
-										values.email
-									)
-								) {
-									errors.email = 'You must supply a valid email address'
-								}
-								return errors
-							}}
-							// Handles our submission
-							onSubmit={this.handleSubmit}
-							//validate={this.handleValidate}
-						>
-							{props => (
-								<Form>
-									<Card>
-										<label htmlFor="email">Email</label>
-										<div>
-											<TablerForm.Input
-												name="email"
-												type="email"
-												placeholder="Enter Email"
-												value={props.values.email}
-												onChange={props.handleChange}
-												onBlur={props.handleBlur}
-												style={{
-													borderColor:
-														props.errors.email && props.touched.email && 'red'
-												}}
-											/>
-											{props.errors.email && props.touched.email && (
-												<div style={{ color: 'red' }}>{props.errors.email}</div>
-											)}
-										</div>
-										<label htmlFor="password">Password</label>
-										<div>
-											<TablerForm.Input
-												name="password"
-												type="password"
-												placeholder="Enter Password"
-												value={props.values.password}
-												onChange={props.handleChange}
-												onBlur={props.handleBlur}
-												style={{
-													borderColor:
-														props.errors.password &&
-														props.touched.password &&
-														'red'
-												}}
-											/>
-
-											{props.errors.password && props.touched.password && (
-												<div style={{ color: 'red' }}>
-													{props.errors.password}
-												</div>
-											)}
-										</div>
-										<Button
-											color="primary"
-											type="submit"
-											disabled={props.isSubmitting}
-										>
-											Submit
-										</Button>
-										&nbsp;
-										<Button
-											color="primary"
-											type="reset"
-											onClick={props.handleReset}
-											disabled={!props.dirty || props.isSubmitting}
-										>
-											Reset
-										</Button>
-									</Card>
-								</Form>
-							)}
-						</Formik>
-					</Grid.Row>
-					<Grid.Row>
-						{message !== '' && (
-							<Alert type="primary">
-								<Alert.Link>{message}</Alert.Link>
-							</Alert>
-						)}
-					</Grid.Row>
-				</Grid.Col>
+				<Formik
+					initialValues={{ email: '', password: '' }}
+					onSubmit={this.handleSubmit}
+					validate={this.handleValidate}
+				>
+					{props => (
+						<Form>
+							<Grid.Col md={7}>
+								<TablerForm.FieldSet>
+									<Alert type="primary">
+										<Alert.Link>Welcome to the AUT Shuttle App!</Alert.Link>
+									</Alert>
+									<TablerForm.Input
+										name="email"
+										type="email"
+										label="Email Address"
+										placeholder="Enter Email"
+										value={props.values.email}
+										onChange={props.handleChange}
+										onBlur={props.handleBlur}
+										style={{
+											borderColor:
+												props.errors.email && props.touched.email && 'red'
+										}}
+									/>
+									{props.errors.email && props.touched.email && (
+										<div style={{ color: 'red' }}>{props.errors.email}</div>
+									)}
+									<TablerForm.Input
+										name="password"
+										type="password"
+										label="Password"
+										placeholder="Enter Password"
+										value={props.values.password}
+										onChange={props.handleChange}
+										onBlur={props.handleBlur}
+										style={{
+											borderColor:
+												props.errors.password && props.touched.password && 'red'
+										}}
+									/>
+									{props.errors.password && props.touched.password && (
+										<div style={{ color: 'red' }}>{props.errors.password}</div>
+									)}
+									&nbsp;
+									<Button
+										color="primary"
+										type="submit"
+										disabled={props.isSubmitting}
+									>
+										Submit
+									</Button>
+									<Button
+										color="warning"
+										type="Register"
+										onClick={() => {
+											this.props.history.push('/register')
+										}}
+									>
+										Register
+									</Button>
+									{message !== '' && (
+										<Alert type="danger">
+											<Alert.Link>{message}</Alert.Link>
+										</Alert>
+									)}
+									<Grid.Row />
+								</TablerForm.FieldSet>
+							</Grid.Col>
+						</Form>
+					)}
+				</Formik>
 			</Page.Content>
 		)
 	}
