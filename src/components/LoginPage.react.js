ea57e57b3921600b5cb4e5aa26b85967e16e22dd
mikegjeff <mikegjeffcoat@gmail.com>
2019-05-01 12:10:30 +1200

    super generic logon screen, need to convert basic html components to tabler

--- src/components/LoginPage.react.js
+++ src/components/LoginPage.react.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Formik } from 'formik'
+import { Formik, Form } from 'formik'
 import { LoginPage as TablerLoginPage } from 'tabler-react'
 import axios from 'axios'
 import API from '../utils/API'
@@ -29,14 +29,17 @@ export default class LoginPage extends Component {
 			.then(result => {
 				localStorage.setItem('token', result.data.token)
 				this.props.history.push('/')
+				setSubmitting(false)
 			})
 			.catch(error => {
 				if (error.response.status === 409) {
+					setSubmitting(false)
 					this.setState({
 						message: 'Login failed. Username or password not match'
 					})
 				}
 				if (error.response.status === 404) {
+					setSubmitting(false)
 					this.setState({
 						message: 'User Not Found'
 					})
@@ -56,51 +59,83 @@ export default class LoginPage extends Component {
 					</Grid.Row>
 					<Grid.Row cards={true}>
 						<Formik
-							initialValues={{
-								email: '',
-								password: ''
-							}}
+							// Sets up our default values
+							initialValues={{ email: '', password: '' }}
+							// Validates our data
 							validate={values => {
-								let errors = {}
-								if (!values.email) {
-									errors.email = 'Required'
-								} else if (
+								const errors = {}
+
+								if (!values.email) errors.email = 'Required'
+
+								if (
 									!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(
 										values.email
 									)
 								) {
-									errors.email = 'Invalid email address'
-								} else if (
-									values.email
-										.split('@')
-										.slice(1)
-										.indexOf('example.com') === -1
-								) {
-									errors.email = 'Not a valid AUT Email'
+									errors.email = 'You must supply a valid email address'
 								}
 								return errors
 							}}
+							// Handles our submission
 							onSubmit={this.handleSubmit}
-							onChange={this.handleChange}
-							render={({
-								values,
-								errors,
-								touched,
-								handleChange,
-								handleBlur,
-								handleSubmit,
-								isSubmitting
-							}) => (
-								<TablerLoginPage
-									onSubmit={handleSubmit}
-									onChange={handleChange}
-									onBlur={handleBlur}
-									values={values}
-									errors={errors}
-									touched={touched}
-								/>
+						>
+							{props => (
+								<Form>
+									<label htmlFor="email">Email</label>
+									<div>
+										<input
+											name="email"
+											type="email"
+											placeholder="Enter Email"
+											value={props.values.email}
+											onChange={props.handleChange}
+											onBlur={props.handleBlur}
+											style={{
+												borderColor:
+													props.errors.email && props.touched.email && 'red'
+											}}
+										/>
+										{props.errors.email && props.touched.email && (
+											<div style={{ color: 'red' }}>{props.errors.email}</div>
+										)}
+									</div>
+									<label htmlFor="password">Password</label>
+									<div>
+										<input
+											name="password"
+											type="password"
+											placeholder="Enter Password"
+											value={props.values.password}
+											onChange={props.handleChange}
+											onBlur={props.handleBlur}
+											style={{
+												borderColor:
+													props.errors.password &&
+													props.touched.password &&
+													'red'
+											}}
+										/>
+										{props.errors.password && props.touched.password && (
+											<div style={{ color: 'red' }}>
+												{props.errors.password}
+											</div>
+										)}
+									</div>
+									<input
+										type="submit"
+										value="Submit"
+										disabled={props.isSubmitting}
+									/>
+									&nbsp;
+									<input
+										type="reset"
+										value="Reset"
+										onClick={props.handleReset}
+										disabled={!props.dirty || props.isSubmitting}
+									/>
+								</Form>
 							)}
-						/>
+						</Formik>
 					</Grid.Row>
 					<Grid.Row>
 						{message !== '' && (
