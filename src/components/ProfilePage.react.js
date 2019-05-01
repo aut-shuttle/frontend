ec432e6537f400ee869f4e6d7915701ca17daf3b
Chenhui Lyu <48456807+ChenhuiLyu1@users.noreply.github.com>
2019-05-01 14:56:34 +1200

    Update ProfilePage.react.js

--- src/components/ProfilePage.react.js
+++ src/components/ProfilePage.react.js
@@ -16,64 +16,55 @@ import {
 	Comment
 } from 'tabler-react'
 
-
-
-
 export default class ProfilePage extends React.Component {
-	 
-	   constructor(props){
-			 super(props)
-		 }
+	constructor(props) {
+		super(props)
+	}
 
-
-		 handleSubmit = async(values, { setSubmitting }) =>{
+	handleSubmit = async (values, { setSubmitting }) => {
 		console.log(values)
 		setSubmitting(true)
 
-    const updateprofile={
-		first_name:values.fname,
-	};
-		api.patch('/profile',updateprofile)
-		
+		const updateprofile = {
+			first_name: values.fname
+		}
+		api.patch('/profile', updateprofile)
 	}
-    render(){
-	return (
-		
-				<SiteWrapper>
-		<div className="profile">
-			<Container>
-				<Grid.Row>
-					<Grid.Col lg={13}>
-						<Form className="card" onSubmit={this.handleSubmit}>
-							<Card.Body>
-								<Card.Title>Profile</Card.Title>
-								<Grid.Row>
-									
-									<Grid.Col sm={6} md={6}>
-										<Form.Group>
-											<Form.Label>First Name</Form.Label>
-											<Form.Input
-												type="text"
-												name="fname"
-												required
-												onChange={this.handleChange}
-											/>
-										</Form.Group>
-									</Grid.Col>
-								</Grid.Row>
-							</Card.Body>
-							<Card.Footer className="text-right">
-								<Button type="submit" color="primary">
-									Upadate profile
-								</Button>
-							</Card.Footer>
-						</Form>
-					</Grid.Col>
-				</Grid.Row>
-			</Container>
-		</div>
-	</SiteWrapper>
-	)
+	render() {
+		return (
+			<SiteWrapper>
+				<div className="profile">
+					<Container>
+						<Grid.Row>
+							<Grid.Col lg={13}>
+								<Form className="card" onSubmit={this.handleSubmit}>
+									<Card.Body>
+										<Card.Title>Profile</Card.Title>
+										<Grid.Row>
+											<Grid.Col sm={6} md={6}>
+												<Form.Group>
+													<Form.Label>First Name</Form.Label>
+													<Form.Input
+														type="text"
+														name="fname"
+														required
+														onChange={this.handleChange}
+													/>
+												</Form.Group>
+											</Grid.Col>
+										</Grid.Row>
+									</Card.Body>
+									<Card.Footer className="text-right">
+										<Button type="submit" color="primary">
+											Upadate profile
+										</Button>
+									</Card.Footer>
+								</Form>
+							</Grid.Col>
+						</Grid.Row>
+					</Container>
+				</div>
+			</SiteWrapper>
+		)
+	}
 }
-}
-
