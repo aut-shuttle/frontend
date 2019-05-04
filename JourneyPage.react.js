b787fc600a59ac1678a0435a640f351ebca5126a
theamiguel <38072446+theamiguel@users.noreply.github.com>
2019-05-04 12:25:54 +1200

    ViewProfile and TopUpPage
    trying to figure out how to commit and publish files... uploaded all my source files.

---
+++ JourneyPage.react.js
@@ -0,0 +1,36 @@
+import * as React from "react";
+import { Stats } from "fs";
+
+
+import SiteWrapper from "../SiteWrapper.react";
+
+
+import {
+    Page,
+    Grid,
+    StatsCard,
+    Card,
+    Profile,
+    Table,
+    ProgressCard,
+    Button
+    
+  } from "tabler-react";
+
+
+function JourneyPage() {
+      return(
+          <SiteWrapper>
+              <Page.Content>
+                    <Grid.Row>
+                        
+                    </Grid.Row>
+              </Page.Content>
+          </SiteWrapper>
+
+
+      )
+
+}
+
+export default JourneyPage;
---
+++ ViewProfile.react.js
@@ -0,0 +1,58 @@
+import * as React from "react";
+import { Stats } from "fs";
+
+
+import SiteWrapper from "../SiteWrapper.react";
+
+
+import {
+    Page,
+    Grid,
+    StatsCard,
+    Card,
+    Profile,
+    Table,
+    ProgressCard,
+    Button,
+    NavLink
+    
+  } from "tabler-react";
+
+
+function ViewProfile() {
+      return(
+          <SiteWrapper>
+              <Page.Content>
+                    <Grid.Row>
+                        <Grid.Col width={20}>
+                            <Grid.Col  md={10} lg={6}>
+                                <Profile 
+                                    name = "Thea Miguel" 
+                                    avatarURL = "./images/avatarthea.jpg" 
+                                    backgroundURL = "./images/aut.jpg" 
+                                    bio = "Bachelor of Computer and Information Sciences" >
+                                </Profile>
+
+                                <Grid.Row cards deck>
+                                    <Grid.Col md={20}>
+                                        <Card body="Student ID: 16946424" />
+                                        <Card body="Email: krk0465@autuni.ac.nz"/>
+                                        
+                                    </Grid.Col>
+                                </Grid.Row>
+                                <Button.List>
+                                    <Button link ="/journeypage" block color="yellow" >Journeys</Button>
+                                    <Button block color="green">Profile Settings</Button>
+                                </Button.List>
+                            </Grid.Col>
+                        </Grid.Col>
+                    </Grid.Row>
+              </Page.Content>
+          </SiteWrapper>
+
+
+      )
+
+}
+
+export default ViewProfile;
