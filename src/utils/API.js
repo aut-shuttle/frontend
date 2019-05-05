396557514e20daba794764303ee6800956f35f9f
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2019-05-06 01:09:09 +1200

    Code formatting

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,5 +3,5 @@ import axios from 'axios'
 export default axios.create({
 	baseURL: 'http://localhost:3000',
 	responseType: 'json',
-	headers: { Authorization: `Bearer ${localStorage.getItem('token') || ""}` }
+	headers: { Authorization: `Bearer ${localStorage.getItem('token') || ''}` }
 })
