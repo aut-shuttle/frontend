393c80ff6f54114831afcf768fe3aa5b0fecb58f
Hamish Maritz <hamishcmaritz@gmail.com>
2019-05-06 19:13:16 +1200

    Update API.js

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-	baseURL: 'http://localhost:3000',
+	baseURL: 'https://aut-staging-api.herokuapp.com',
 	responseType: 'json',
 	headers: { Authorization: `Bearer ${localStorage.getItem('token') || ''}` }
 })
