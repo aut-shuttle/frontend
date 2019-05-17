e99a74bf7665fd3b47edf7059b76719ba583be92
Hamish Maritz <hamishcmaritz@gmail.com>
2019-05-17 15:56:26 +1200

    changes to manifest and index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App.react'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-
+// https://github.com/facebook/create-react-app/issues/2272#issuecomment-302832432
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
