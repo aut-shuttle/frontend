3d45ffa2c07928eb10fcdbca1d89823b16a45120
ChenhuiLyu1 <48456807+ChenhuiLyu1@users.noreply.github.com>
2019-05-18 23:53:56 +1200

    update

--- src/components/busschedule/BusSchedulePage.react.js
+++ src/components/busschedule/BusSchedulePage.react.js
@@ -1,59 +1,56 @@
 import React, { Component } from 'react'
-import { Formik, Form } from 'formik'
-import API from '../../utils/API'
+
 import { Page, Grid, Alert, Form as TablerForm, Button } from 'tabler-react'
 import SiteWrapper from '../../SiteWrapper.react'
-import Time from 'react-time'
-import { StyleSheet, View, Text } from 'react-native';
-export default class BusSchedulePage extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-          //defauilt value of the date time
-          date: '',
-        };
-      }
-      componentDidMount() {
-        var that = this;
-        var date = new Date().getDate(); //Current Date
-        var month = new Date().getMonth() + 1; //Current Month
-        var year = new Date().getFullYear(); //Current Year
-        var hours = new Date().getHours(); //Current Hours
-        var min = new Date().getMinutes(); //Current Minutes
-        var sec = new Date().getSeconds(); //Current Seconds
-        that.setState({
-          //Setting the value of the date time
-          date:
-            date + '/' + month + '/' + year + ' ' + hours + ':' + min + ':' + sec,
-        });
-      }
-    render(){
-        setInterval(function(){this.setState({curTime: new  Date().toLocaleString()});}.bind(this), 1000);
-        return(
-        <SiteWrapper>
-         <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <Text
-          style={{
-            fontSize: 20,
-          }}>
-          Current Date Time
-        </Text>
-        <Text
-          style={{
-            fontSize: 20,
-            marginTop: 16,
-          }}>
-          {this.state.date}
-        </Text>
-      </View>
-        </SiteWrapper>
-        )
-        
-    }
+
+export default class BusSchedulePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+  compare() {
+    
+  }
+
+
+  render() {
+
+    return (
+      <SiteWrapper>
+        <Page.Content
+          title={
+            'Current Time：' + this.state.date.toLocaleTimeString()
+          }>
+          <Button
+            block
+            color="warning"
+            type="Back"
+            onClick={this.compare()}
+          >
+            Back
+										</Button>
+        </Page.Content>
+
+      </SiteWrapper>
+    )
+
+  }
 }
