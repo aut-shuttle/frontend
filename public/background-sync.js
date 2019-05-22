48b4b675085fefbae90a10634fe1e6748e82bc3b
hamishmaritz <hamishcmaritz@gmail.com>
2019-05-23 00:35:22 +1200

    Added Extras

---
+++ public/background-sync.js
@@ -0,0 +1,24 @@
+//Simple background sync
+self.addEventListener('sync', function (event) {
+    console.log('firing: sync');
+    if (event.tag === 'myFirstSync') {
+        event.waitUntil(fetchRandomImage());
+    }
+});
+
+function fetchRandomImage() {
+    //https://unsplash.it/200/300/?random
+    console.log('fetching random Image');
+    fetch('https://thecatapi.com/api/images/get?format=src&type=gif')
+        .then((response) => {
+            console.log(response);
+            self.registration.showNotification('Hey! New image!',{body:response.url,image:response.url});
+            return response;
+        })
+        .then((text) => {
+            console.log('Request successfull ' + text);
+        })
+        .catch((error) => {
+            console.log('Request failed ' + error);
+    });
+}
---
+++ sw-precache-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    stripPrefix: 'build/',
+    staticFileGlobs: [
+      'build/*.html',
+      'build/manifest.json',
+      'build/static/**/!(*map*)'
+    ],
+    importScripts: [
+        'background-sync.js'
+    ],
+    dontCacheBustUrlsMatching: /\.\w{8}\./,
+    swFilePath: 'build/service-worker.js'
+  };
